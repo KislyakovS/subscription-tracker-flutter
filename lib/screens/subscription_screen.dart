a9d60f6fda2d30249983e07c28713fcae0e9509f
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-08 17:17:17 +0300

    Added popup menu action sub

--- lib/screens/subscription_screen.dart
+++ lib/screens/subscription_screen.dart
@@ -1,207 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:subscription_tracker/config/palette.dart';
 import 'package:subscription_tracker/widgets/widgets.dart';
 
 class SubscriptionScreen extends StatelessWidget {
-  final TextEditingController _titleController =
-      TextEditingController(text: 'Netflix');
-  final TextEditingController _priceController =
-      TextEditingController(text: '12.99');
-  final TextEditingController _currencyController =
-      TextEditingController(text: 'USD (\$)');
-  final TextEditingController _periodController =
-      TextEditingController(text: 'Every 1 month');
-  final TextEditingController _dateController =
-      TextEditingController(text: '10.10.2021');
-  final TextEditingController _notificationsController =
-      TextEditingController(text: 'Yes');
-
-  void _showModal(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-      ),
-      backgroundColor: Palette.background,
-      builder: (context) {
-        return DraggableScrollableSheet(
-          expand: false,
-          maxChildSize: 0.92,
-          initialChildSize: 0.92,
-          minChildSize: 0.92,
-          builder: (context, scrollController) {
-            return Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // IconButton(
-                      //   icon: Icon(
-                      //     CupertinoIcons.chevron_down,
-                      //     size: 30,
-                      //   ),
-                      //   onPressed: () => Navigator.of(context).pop(),
-                      // ),
-                      Flexible(
-                          child: Text(
-                        'Netflix',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22,
-                        ),
-                      )),
-                      // TextButton(
-                      //   onPressed: null,
-                      //   child: Text(
-                      //     'Save',
-                      //     style: TextStyle(fontSize: 18),
-                      //   ),
-                      // )
-                    ],
-                  ),
-                  const SizedBox(height: 25),
-                  Column(
-                    children: [
-                      // Container(
-                      //   width: 150,
-                      //   height: 150,
-                      //   decoration: BoxDecoration(
-                      //     color: Colors.grey[300],
-                      //     borderRadius: BorderRadius.circular(10),
-                      //   ),
-                      // ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          'https://cdn.vox-cdn.com/thumbor/QuS2QKQys3HhosKiV-2IuKhphbo=/39x0:3111x2048/1400x1050/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png',
-                          width: 150,
-                          height: 150,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const SizedBox(height: 50),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Title',
-                          ),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: 'Hello',
-                              controller: _titleController,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Colors.grey[400]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Price'),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: '100',
-                              controller: _priceController,
-                              keyboardType: TextInputType.number,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Colors.grey[400]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Currency'),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: 'USD',
-                              controller: _currencyController,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Colors.grey[400]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Period'),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: 'Every 1 month',
-                              controller: _periodController,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Colors.grey[400]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Date'),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: '29.10.2021',
-                              controller: _dateController,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(color: Colors.grey[400]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Notifications'),
-                          Expanded(
-                            child: CupertinoTextField(
-                              placeholder: 'No',
-                              controller: _notificationsController,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.right,
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Colors.transparent),
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          },
-        );
-      },
+  Widget _openMenu(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: (context) => [PopupMenuItem(child: Text('12312311'))],
     );
   }
 
@@ -216,8 +20,19 @@ class SubscriptionScreen extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
         actions: [
-          IconButton(
-              icon: Icon(Icons.edit), onPressed: () => _showModal(context))
+          PopupMenuButton(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Icon(Icons.more_vert),
+            ),
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(child: Text('Edit'),),
+                PopupMenuItem(child: Text('Paused')),
+                PopupMenuItem(child: Text('Delete')),
+              ];
+            },
+          ),
         ],
       ),
       body: CustomScrollView(
