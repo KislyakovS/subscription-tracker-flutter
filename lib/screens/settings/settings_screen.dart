ebdfe1a7935d0f75f0dd6b463d814735d602cb79
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-11 10:01:45 +0300

    Added scroll shadow settings screen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,10 +1,8 @@
-// import 'package:currency_picker/currency_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:subscription_tracker/config/constants.dart';
 import 'package:subscription_tracker/screens/settings/components/form_subscription_request.dart';
 import 'package:subscription_tracker/screens/settings/components/settings.dart';
-// import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -12,6 +10,34 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  int _indexSliding = 0;
+  final double targetElevation = 3;
+  double _elevation = 0;
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.removeListener(_scrollListener);
+    _controller.dispose();
+  }
+
+  void _scrollListener() {
+    double newElevation = _controller.offset > 1 ? targetElevation : 0;
+    if (_elevation != newElevation) {
+      setState(() {
+        _elevation = newElevation;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +50,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
       ),
       body: SingleChildScrollView(
+        controller: _controller,
         padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
         child: Column(
           children: [
@@ -36,154 +63,3 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 }
-
-// class SettingsScreen extends StatelessWidget {
-//   // Future<Null> selectTime(BuildContext context) async {
-//   //   var picked = await showTimePicker(
-//   //     context: context,
-//   //     initialTime: TimeOfDay.now(),
-//   //     builder: (context, child) {
-//   //       return CupertinoTimerPicker(
-//   //         onTimerDurationChanged: (time) {},
-//   //         backgroundColor: Colors.white,
-//   //       );
-//   //     },
-//   //   );
-//   // }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         elevation: 0,
-//         centerTitle: false,
-//         title: Text(
-//           'Settings',
-//           style: TextStyle(fontSize: 25, color: Colors.black),
-//         ),
-//       ),
-//       body: CustomScrollView(
-//         slivers: [
-//           SliverToBoxAdapter(
-//             child: Container(
-//               margin: const EdgeInsets.only(top: 5),
-//               padding: const EdgeInsets.symmetric(horizontal: 20),
-//               child: Container(
-//                 padding: const EdgeInsets.all(20),
-//                 decoration: BoxDecoration(
-//                   color: Colors.white,
-//                   borderRadius: BorderRadius.circular(20),
-//                 ),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(vertical: 4),
-//                       child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text(
-//                             'Notifications',
-//                             style: TextStyle(fontSize: 16),
-//                           ),
-//                           CupertinoSwitch(value: true, onChanged: (value) {})
-//                         ],
-//                       ),
-//                     ),
-//                     Divider(color: Colors.grey[400]),
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(vertical: 7),
-//                       child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text(
-//                             'Notification time',
-//                             style: TextStyle(fontSize: 16),
-//                           ),
-//                           Text(
-//                             '10:10',
-//                             style: TextStyle(
-//                                 fontSize: 16, fontWeight: FontWeight.bold),
-//                           )
-//                         ],
-//                       ),
-//                     ),
-//                     Divider(color: Colors.grey[400]),
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(vertical: 7),
-//                       child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text(
-//                             'Default currency',
-//                             style: TextStyle(fontSize: 16),
-//                           ),
-//                           Text(
-//                             'USD (\$)',
-//                             style: TextStyle(
-//                                 fontSize: 16, fontWeight: FontWeight.bold),
-//                           )
-//                         ],
-//                       ),
-//                     ),
-//                     Divider(color: Colors.grey[400]),
-//                     Padding(
-//                       padding: const EdgeInsets.symmetric(vertical: 4),
-//                       child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           Text(
-//                             'Show paused items',
-//                             style: TextStyle(fontSize: 16),
-//                           ),
-//                           CupertinoSwitch(value: true, onChanged: (value) {})
-//                         ],
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               margin: const EdgeInsets.only(top: 20),
-//               padding: const EdgeInsets.symmetric(horizontal: 20),
-//               child: Container(
-//                 padding: const EdgeInsets.all(20),
-//                 decoration: BoxDecoration(
-//                   color: Colors.white,
-//                   borderRadius: BorderRadius.circular(20),
-//                 ),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     Text(
-//                       'Leave a subscription request',
-//                       style:
-//                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-//                     ),
-//                     const SizedBox(height: 20),
-//                     CupertinoTextField(
-//                       placeholder: 'Service name',
-//                     ),
-//                     const SizedBox(height: 15),
-//                     CupertinoTextField(
-//                       placeholder: 'Service url',
-//                     ),
-//                     const SizedBox(height: 15),
-//                     CupertinoButton(
-//                       child: Text('Send'),
-//                       onPressed: () {},
-//                       color: Colors.purple,
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
