509b70d40ebf03797fcea1c4a4effbf8826d29c9
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-12 09:19:01 +0300

    Added shadow form screen

--- lib/screens/form_subscription/form_subscription_screen.dart
+++ lib/screens/form_subscription/form_subscription_screen.dart
@@ -11,85 +11,121 @@ class FormItem {
   FormItem({required this.label, required this.field});
 }
 
-class FormSubscriptionScreen extends StatelessWidget {
+class FormSubscriptionScreen extends StatefulWidget {
   final Subscription subscription;
 
   final List<FormItem> formItems = [
     FormItem(
-        label: 'Title',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        )),
+      label: 'Title',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
     FormItem(
-        label: 'Price',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        )),
+      label: 'Price',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
     FormItem(
-        label: 'Currency',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        )),
+      label: 'Currency',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
     FormItem(
-        label: 'Period',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        )),
+      label: 'Period',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
     FormItem(
-        label: 'Date',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        )),
+      label: 'Date',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
     FormItem(
-        label: 'Notifications',
-        field: CupertinoTextField(
-          placeholder: 'Hello',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.right,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.transparent),
-          ),
-        ))
+      label: 'Notifications',
+      field: CupertinoTextField(
+        placeholder: 'Hello',
+        style: const TextStyle(fontWeight: FontWeight.bold),
+        textAlign: TextAlign.right,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.transparent),
+        ),
+      ),
+    ),
   ];
 
-  FormSubscriptionScreen({
-    Key? key,
-    required this.subscription,
-  }) : super(key: key);
+  FormSubscriptionScreen({Key? key, required this.subscription})
+      : super(key: key);
+
+  @override
+  _FormSubscriptionScreenState createState() => _FormSubscriptionScreenState();
+}
+
+class _FormSubscriptionScreenState extends State<FormSubscriptionScreen> {
+  final double targetElevation = 3;
+  double _elevation = 0;
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    double newElevation = _controller.offset > 1 ? targetElevation : 0;
+    if (_elevation != newElevation) {
+      setState(() {
+        _elevation = newElevation;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.removeListener(_scrollListener);
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
+        elevation: _elevation,
         iconTheme: const IconThemeData(color: Colors.black),
         title: Text(
-          subscription.title,
+          widget.subscription.title,
           style: const TextStyle(color: Colors.black),
         ),
         actions: [
@@ -103,19 +139,21 @@ class FormSubscriptionScreen extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
+        controller: _controller,
         padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             Column(
               children: [
-                ImageSubscription(src: subscription.image, size: 150),
+                ImageSubscription(src: widget.subscription.image, size: 150),
                 const SizedBox(height: 50),
-                ...formItems
+                ...widget.formItems
                     .asMap()
                     .entries
                     .map((item) => _RowForm(
                           item: item.value,
-                          isShowDivider: item.key != formItems.length - 1,
+                          isShowDivider:
+                              item.key != widget.formItems.length - 1,
                         ))
                     .toList()
               ],
